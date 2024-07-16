118a262c567d1562a2fff102b62f6a2b8d16e1bd
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2024-07-16 16:46:44 -0300

    chore: version

--- eitri-app.conf.js
+++ eitri-app.conf.js
@@ -7,7 +7,7 @@ module.exports = {
     "public-key": "65ae588e-3fb8-4545-9773-9b5bd8bb41e6",
     "organizationId": "cf5660ee-bf90-42cd-9a43-9d2c69ee3c89",
     "applicationId": "749d6f6f-f10f-4448-b36e-9c484b1293b8",
-    "version": "0.1.22",
+    "version": "0.1.43",
     // "eitri-luminus": "@cea-components:1.0.5",
     "eitri-luminus": "1.40.5",
     "eitri-bifrost": "2.0.0",
