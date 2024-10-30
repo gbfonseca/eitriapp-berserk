69a2eb877f8af7214a55eac31d304dc7483bba70
Gabriel Fonseca <gabriel.silva@calindra.com.br>
2024-10-30 15:39:23 -0300

    fix: try bump 1.0.12

--- eitri-app.conf.js
+++ eitri-app.conf.js
@@ -5,7 +5,7 @@ module.exports = {
     "eitri-luminus": "1.40.5",
     "eitri-bifrost": "2.0.0",
     "eitri-commons": "1.0.0",
-    "version": "1.0.10",
+    "version": "1.0.11",
     "public-key": "65ae588e-3fb8-4545-9773-9b5bd8bb41e6",
     "applicationId": "749d6f6f-f10f-4448-b36e-9c484b1293b8",
     "id": "a861465b-8bf5-416f-8599-90cdb95c6099",
