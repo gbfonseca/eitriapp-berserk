cb4875eb31cb8f25ae3ca1d8335e53153dd0f727
Gabriel Fonseca <gabriel.silva@calindra.com.br>
2024-10-30 18:08:52 -0300

    fix: bump 1.0.42

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -57,7 +57,7 @@ export default function Home(props) {
             By: Kentaro Miura
           </Text>
           <Text fontSize="big" fontWeight="bold">
-            v1.0.41
+            v1.0.42
           </Text>
         </View>
         <Text
